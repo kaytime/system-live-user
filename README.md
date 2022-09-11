e6fd8f5bf0afb90c2b678e7912a3506819b5b726
Steph DGenius <stephanetse@gmail.com>
2022-09-11 21:25:34 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kaytime Live User [![Build Status](https://travis-ci.org/kaytime/kaytime-live-user.svg?branch=master)](https://travis-ci.org/kaytime/kaytime-live-user)
+
+Package to create the system's default user.
+
+# Issues
+
+If you find problems with the contents of this repository please create an issue.
+
+©2022 Kaytime Labs
---
+++ gh-build.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+apt-get --yes update
+apt-get --yes install wget equivs curl git
+
+git_commit=$(git rev-parse --short HEAD)
+
+printf >config "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://kaytime.github.io" \
+	"Package: system-live-user" \
+	"Version: $git_commit" \
+	"Maintainer: Steph DGenius <stephanetse@gmail.com>" \
+	"Depends: adduser" \
+	"Architecture: all" \
+	"Postinst: postinst" \
+	"Postrm: postrm" \
+	"Description: Default user for the Kaytime system."
+
+equivs-build config
